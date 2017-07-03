b6ff15bbc9a1155664893a7fed4d18f3a9a1c6d1
Rory Dahl <rsdah13@gmail.com>
2017-07-03 13:47:42 +0200

    Academy fork

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 
+var About = require('./components/about/aboutPage')//hardcode, without the react-router library
+
 React.render(<Home />, document.getElementById('app'));
