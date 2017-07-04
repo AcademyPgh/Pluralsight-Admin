4bb63242558dc610d8dd7ff8bf68cee3f67f827f
Rory Dahl <rsdah13@gmail.com>
2017-07-04 09:28:04 +0200

    update

--- src/main.js
+++ src/main.js
@@ -4,4 +4,32 @@ var Home = require('./components/homePage');
 
 var About = require('./components/about/aboutPage')//hardcode, without the react-router library
 
+var App = React.createClass({
+  render: function () {
+    var Child;
+
+    switch (this.props.route) {
+      case 'about':
+        Child = About;
+        break;
+      default:
+        Child = Home;
+
+    }
+    return (
+      <div>
+        <Child />
+      </div>
+    );
+  }
+});
+
+function render() {
+  var route = window.location.hash.substr(1);
+  React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
+
 React.render(<Home />, document.getElementById('app'));
